0752f4ed08e6436dabad34a712b093c5e1fde085
zahraabdulova <zabdulova16110@ada.edu.az>
2026-03-06 09:14:15 +0400

    Create  Circle.java

---
+++ Circle.java
@@ -0,0 +1,14 @@
+ public class Circle {
+
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public String toString() {
+        return "Circle center: " + center + " radius: " + radius;
+    }
+}
