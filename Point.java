15808db50d3a717cff7502e816a0c37379f84440
zahraabdulova <zabdulova16110@ada.edu.az>
2026-03-06 09:07:05 +0400

    Create  Point.java

---
+++ Point.java
@@ -0,0 +1,30 @@
+public class Point implements Movable {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void moveUp() {
+        y++;
+    }
+
+    public void moveDown() {
+        y--;
+    }
+
+    public void moveLeft() {
+        x--;
+    }
+
+    public void moveRight() {
+        x++;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
